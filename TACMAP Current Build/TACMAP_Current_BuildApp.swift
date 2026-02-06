98366461c31c9dc25012fefda399057f593bb174
Olibri25 <mathwich.b@gmail.com>
2026-02-05 19:41:25 -0900

    Initial Commit

---
+++ TACMAP Current Build/TACMAP_Current_BuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  TACMAP_Current_BuildApp.swift
+//  TACMAP Current Build
+//
+//  Created by Brian Mathwich on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TACMAP_Current_BuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
